b233f244763e98fbb6e70ac6b27d0c411791724f
emmanueljucaban <emmanueljucaban@gmail.com>
2020-10-21 22:00:40 -0700

    F - Wired up get request with the database

---
+++ routes/apiRoutes/userRoutes/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {findAllUsersFromDb} = require('../../../model/userOrm');
+// /api/users
+
+router.route('/')
+  .get(async (req, res) => {
+    try {
+      const users = await findAllUsersFromDb();
+      res.json(users);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  })
+
+
+module.exports = router;
